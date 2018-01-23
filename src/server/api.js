8e106b554a0f06225c5ef7cbcbd2a00d62a506ae
pipetit1 <petit.pierre09@gmail.com>
2018-01-23 17:11:29 +0100

    add method create list get delete

--- src/server/api.js
+++ src/server/api.js
@@ -13,14 +13,80 @@ router.get('/', function(req, res) {
 });
 
 router.get('/episodes', function(req, res) {
-    res.send('hey');
+    var files = fs.readdirSync("./data");
+    var episodes = [];
+
+    files.forEach((elt) => {
+        if(elt.split('.').pop() == 'json'){
+            var data = fs.readFileSync('./data/' + elt);
+            var parsed = JSON.parse(data);
+            episodes.push({
+                id : elt.split('.')[0],
+                name : parsed.name,
+                code : parsed.code,
+                score : parsed.score,
+            });
+        }
+    });
+
+    res.send(episodes);
 });
 
 // define the about route
-router.post('/episodes', function(req, res) {
-    fs.writeFile("data/"+uuid.v4()+".json", JSON.stringify(req.query));
-    res.send(req.query);
+router.post('/episodes/add', function(req, res) {
+    var id = uuid.v4();
+    var episode = req.query;
+    fs.writeFile("data/"+id+".json", JSON.stringify(episode));
+    res.send({
+        id : id,
+        name : episode.name,
+        code : episode.code,
+        score : episode.score
+    });
 });
 
+router.get("/episode", function (req, res) {
+    var files = fs.readdirSync("./data");
+    var id = req.query.id;
+    var episode = {};
+    files.forEach((elt) => {
+        var fragments = elt.split('.');
+        if(fragments.pop() == 'json' && fragments[0] == id){
+            var data = fs.readFileSync('./data/' + elt);
+            var parsed = JSON.parse(data);
+            episode = {
+                id : elt.split('.')[0],
+                name : parsed.name,
+                code : parsed.code,
+                score : parsed.score,
+            };
+        }
+    });
+
+    res.send(episode);
+});
+
+router.delete("/episode", function (req, res) {
+    var files = fs.readdirSync("./data");
+    var id = req.query.id;
+    var episode = {};
+    files.forEach((elt) => {
+        var fragments = elt.split('.');
+        if(fragments.pop() == 'json' && fragments[0] == id){
+            var data = fs.readFileSync('./data/' + elt);
+            var parsed = JSON.parse(data);
+            episode = {
+                id : elt.split('.')[0],
+                name : parsed.name,
+                code : parsed.code,
+                score : parsed.score,
+            };
+
+            fs.unlinkSync('./data/' + elt);
+        }
+    });
+
+    res.send(episode);
+});
 module.exports = router;
 
