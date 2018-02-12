ce90e4c686dc105f2452871b504860462aab3060
Ferrere Valentin <gadoy512@gmail.com>
2018-02-12 23:15:00 +0100

    full async calls in the dal

--- src/server/dal.js
+++ src/server/dal.js
@@ -7,62 +7,66 @@ const dal = require('./dal.js');
 
 function readFile(fileName) {
     return new Promise((resolve, reject) => {
-        if(fileName.split('.').pop() == 'json'){
+        if (fileName.split('.').pop() == 'json') {
             console.log(fileName);
-             fs.readFile(config.data + "/" + fileName, (err, data) => {
-                 if(err) reject(err);
-                 else {
-                     resolve(JSON.parse(data));
-                 }
-             });
+            fs.readFile(config.data + "/" + fileName, (err, data) => {
+                if (err) reject(err);
+                else {
+                    resolve(JSON.parse(data));
+                }
+            });
         }
     })
 
 }
 
-module.exports.getAll = function() {
-    var files = fs.readdirSync(config.data);
-    var promises = [];
-
-
+module.exports.getAll = function () {
     return new Promise((resolve, reject) => {
-        files.forEach(function(elt) {
-            var fragments = elt.split('.');
-            promises.push(dal.getById(fragments[0]));
-        });
+        fs.readdir(config.data, (err, files) => {
+            var promises = [];
 
-        Promise.all(promises).then((episodes) => {
-            resolve(episodes);
-        }).catch((err) => {
-            reject("err");
-        });
-    })
+
+            files.forEach(function (elt) {
+                var fragments = elt.split('.');
+                promises.push(dal.getById(fragments[0]));
+            });
+
+            Promise.all(promises).then((episodes) => {
+                resolve(episodes);
+            }).catch((err) => {
+                reject(err);
+            });
+        })
+    });
+
 };
 
-module.exports.getById = function(id) {
-    var files = fs.readdirSync(config.data);
+module.exports.getById = function (id) {
     var episode;
     return new Promise((resolve, reject) => {
-        files.forEach(function(elt) {
-            var fragments = elt.split('.');
-            if(fragments.pop() == 'json' && fragments[0] == id){
-                readFile(elt).then((parsed) => {
-                    episode = {
-                        id : id,
-                        name : parsed.name,
-                        code : parsed.code,
-                        score : parsed.score,
-                    };
-                    resolve(episode);
-                }).catch((err) => {
-                    reject(err);
-                });
-            }
-        });
+        fs.readdir(config.data, (err, files) => {
+            files.forEach(function (elt) {
+                var fragments = elt.split('.');
+                if (fragments.pop() == 'json' && fragments[0] == id) {
+                    readFile(elt).then((parsed) => {
+                        episode = {
+                            id: id,
+                            name: parsed.name,
+                            code: parsed.code,
+                            score: parsed.score,
+                        };
+                        resolve(episode);
+                    }).catch((err) => {
+                        reject(err);
+                    });
+                }
+            });
+        })
+
     });
 };
 
-module.exports.insert = function(episode, id) {
+module.exports.insert = function (episode, id) {
     return new Promise((resolve, reject) => {
         fs.writeFile(config.data + "/" + id + ".json", JSON.stringify(episode));
         console.log(episode);
@@ -70,12 +74,12 @@ module.exports.insert = function(episode, id) {
     });
 };
 
-module.exports.delete = function(id) {
+module.exports.delete = function (id) {
     return new Promise((resolve, reject) => {
         this.getById(id).then((episode) => {
             fs.unlink(config.data + "/" + id + ".json", (err) => {
 
-                if(err) {
+                if (err) {
                     reject(err);
                 } else {
                     console.log(episode);
@@ -88,11 +92,11 @@ module.exports.delete = function(id) {
     });
 };
 
-module.exports.update = function(id, body) {
+module.exports.update = function (id, body) {
     return new Promise((resolve, reject) => {
         this.getById(id).then((episode) => {
-            for (var key in body){
-                if (episode.hasOwnProperty(key)){
+            for (var key in body) {
+                if (episode.hasOwnProperty(key)) {
                     console.log(key)
                     episode[key] = body[key];
                 }
